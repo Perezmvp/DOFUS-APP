d98f0269062b099dd99d5c8dfa5140f445898a04
Perezmvp <perez14sebastian@gmail.com>
2023-03-05 17:36:17 -0500

    Update Manitas.py

--- Manitas.py
+++ Manitas.py
@@ -46,7 +46,7 @@ TODOS=TODOS.sort_values(by='Rentabilidad', ascending=False)
 
 if OPCIONES == 'Todos':
     st.subheader('TODOS :earth_americas:')
-    st.write(TODOS.reset_index(inplace=True))
+    st.write(TODOS)
 if OPCIONES == 'Acariciadoras':
     st.subheader('ACARICIADORAS :smile:')
     st.write(ACARICIADORAS.reset_index())
