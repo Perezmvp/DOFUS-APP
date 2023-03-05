8f5e8579c26a3dc89b516adf33dcd3036f2c92aa
Perezmvp <127048244+Perezmvp@users.noreply.github.com>
2023-03-05 14:05:13 -0500

    Add files via upload

---
+++ Manitas.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+
+st.title('Rentabilidad de objetos de cria')
+
+st.subheader('Raw data')
+DATA=pd.read_csv('DATA.csv', sep=';')
+DATA['Rendimiento']=DATA['Eficacia']*DATA['Usos']
+DATA['R/P']=DATA['Rendimiento']/DATA['Precio']
+
+
+DATA1=DATA.query("Item=='Abrevadero'")
+DATA1RP=DATA1['R/P']
+DATA2=DATA.query("Item=='Aporreadora'")
+DATA2RP=DATA2['R/P']
+DATA1MEDIAN=DATA1RP.median()
+DATA2MEDIAN=DATA2RP.median()
+#DATA1ORDENADO=DATA1.sort_values(by='Nivel', ascending=False)
+
+st.write(DATA)
+st.write(DATA1)
+st.write(DATA2)
+st.write(DATA1MEDIAN)
+st.write(DATA2MEDIAN)
