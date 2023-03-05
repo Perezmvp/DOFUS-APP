e4530a05a51e9ef45929ad3ec6434e2ffa88af34
Perezmvp <127048244+Perezmvp@users.noreply.github.com>
2023-03-05 14:13:30 -0500

    Add files via upload

--- Manitas.py
+++ Manitas.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.title('Rentabilidad de objetos de cria')
 
-st.subheader('Raw data')
+st.subheader('DATOS')
 DATA=pd.read_csv('DATA.csv', sep=';')
 DATA['Rendimiento']=DATA['Eficacia']*DATA['Usos']
 DATA['R/P']=DATA['Rendimiento']/DATA['Precio']
@@ -18,7 +18,9 @@ DATA2MEDIAN=DATA2RP.median()
 #DATA1ORDENADO=DATA1.sort_values(by='Nivel', ascending=False)
 
 st.write(DATA)
+st.subheader('Abrevaderos')
 st.write(DATA1)
+st.subheader('Aporreadoras')
 st.write(DATA2)
 st.write(DATA1MEDIAN)
 st.write(DATA2MEDIAN)
