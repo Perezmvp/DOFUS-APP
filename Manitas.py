0a70fa766cf811766c155c240e77df9fe2219872
Perezmvp <perez14sebastian@gmail.com>
2023-03-05 17:14:30 -0500

    Update Manitas.py

--- Manitas.py
+++ Manitas.py
@@ -6,8 +6,7 @@ st.title('Rentabilidad de objetos de cria')
 NOMBRE_OPCIONES=['Todos','Acariciadoras','Aporreadoras','Dragonalgas','Abrevaderos','Fulminadoras']
 OPCIONES=st.radio('Filtrar por:',NOMBRE_OPCIONES)
 
-DATAINDEX=pd.read_csv('DATA.csv', sep=';')
-DATA=DATAINDEX.style.hide_index()
+DATA=pd.read_csv('DATA.csv', sep=';')
 st.write(DATA)
 DATA['Rendimiento']=DATA['Eficacia']*DATA['Usos']
 DATA['R/P']=DATA['Rendimiento']/DATA['Precio']
