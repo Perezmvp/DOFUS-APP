7cd7d1dec81649f926e41242a8e85d5dad594cd0
Perezmvp <perez14sebastian@gmail.com>
2023-03-05 22:09:30 -0500

    Update Manitas.py

--- pages/Manitas.py
+++ pages/Manitas.py
@@ -70,7 +70,7 @@ with tab1:
         st.write(FULMINADORAS.reset_index(drop=True))
 
 with st.form('Registro1'):
-    del NOMBRE_OPCIONES[1]
+    del NOMBRE_OPCIONES[0]
     st.selectbox('Item', NOMBRE_OPCIONES)
 
 
