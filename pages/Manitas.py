a11c844bf675edb9ef61346d85a75d56a3220bd6
Perezmvp <perez14sebastian@gmail.com>
2023-03-05 21:25:31 -0500

    Update Manitas.py

--- pages/Manitas.py
+++ pages/Manitas.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 st.title('Rentabilidad de objetos de cria')
-tab1, tab2, tab3 = st.tabs(["Busqueda", "Agregar entrada"])
+tab1, tab2= st.tabs(["Busqueda", "Agregar entrada"])
 with tab1:
     NOMBRE_OPCIONES=['Todos','Acariciadoras','Aporreadoras','Dragonalgas','Abrevaderos','Fulminadoras']
     OPCIONES=st.radio('Filtrar por:',NOMBRE_OPCIONES,horizontal=True)
