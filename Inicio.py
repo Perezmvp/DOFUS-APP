313521b383517aa15647d62409e57e6697c3f8e9
Perezmvp <perez14sebastian@gmail.com>
2023-03-07 12:04:13 -0500

    No comment

--- Inicio.py
+++ Inicio.py
@@ -1,1 +1,6 @@
 import streamlit as st
+import gspread
+
+gc = gspread.service_account(filename='DOFUS TOOLS API KEY.json')
+Manitas = gc.open('DATA').worksheet('CRIA')
+Manitas.update_cell(20,1, 'SEBASTIAN')
