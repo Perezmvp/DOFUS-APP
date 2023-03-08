06abe2150d49ebb172b62f473d53203bdc1e6e7b
Perezmvp <perez14sebastian@gmail.com>
2023-03-07 19:48:57 -0500

    Update Inventario de monturas.py

--- pages/Inventario de monturas.py
+++ pages/Inventario de monturas.py
@@ -5,10 +5,9 @@ tab1, tab2= st.tabs(["All-might-man", "Jalettyz"])
 st.title('Rentabilidad de objetos de cria')
 
 with tab1:
-	gc = gspread.service_account(filename='DOFUS TOOLS API KEY.json')
-	INV_PEREZ = gc.open('DATA').worksheet('INV_PÉREZ')
-	DATAINVPEREZ = pd.DataFrame(INV_PEREZ.get_all_records())
-
+    gc = gspread.service_account(filename='DOFUS TOOLS API KEY.json')
+    INV_PEREZ = gc.open('DATA').worksheet('INV_PÉREZ')
+    DATAINVPEREZ = pd.DataFrame(INV_PEREZ.get_all_records())
     st.write(DATAINVPEREZ)
 
 
