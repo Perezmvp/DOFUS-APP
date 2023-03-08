c35fe6a04c58ab95f5f4e58855a1bd4f89054523
Perezmvp <perez14sebastian@gmail.com>
2023-03-07 19:41:59 -0500

    Update Inventario de monturas.py

---
+++ pages/Inventario de monturas.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import gspread
+
+tab1, tab2= st.tabs(["All-might-man", "Jalettyz"])
+
+with tab1:
+	gc = gspread.service_account(filename='DOFUS TOOLS API KEY.json')
+    INV_PEREZ = gc.open('DATA').worksheet('INV_PÉREZ')
+    DATAINVPEREZ = pd.DataFrame(INV_PEREZ.get_all_records())
+
+    st.write(DATAINVPEREZ)
+
+
+
+
