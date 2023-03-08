99cf5a5d8588e66fa5ba47e127b30fe095693fa3
Perezmvp <perez14sebastian@gmail.com>
2023-03-07 19:45:46 -0500

    Update Inventario de monturas.py

--- pages/Inventario de monturas.py
+++ pages/Inventario de monturas.py
@@ -1,12 +1,8 @@
 import streamlit as st
 import pandas as pd
 import gspread
-
 tab1, tab2= st.tabs(["All-might-man", "Jalettyz"])
-
 st.title('Rentabilidad de objetos de cria')
-
-
 with tab1:
 	gc = gspread.service_account(filename='DOFUS TOOLS API KEY.json')
 	INV_PEREZ = gc.open('DATA').worksheet('INV_PÉREZ')
